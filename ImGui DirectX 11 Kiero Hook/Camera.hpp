3519b9723abc27d840861d2c3da0014c009336d1
INFARCTED <93995570+INFARCTED@users.noreply.github.com>
2023-04-24 12:47:54 +0200

    cg_fovmin !

--- ImGui DirectX 11 Kiero Hook/Camera.hpp
+++ ImGui DirectX 11 Kiero Hook/Camera.hpp
@@ -5,13 +5,25 @@ void Camera_Tab()
 		ImGui::SetCursorPos(ImVec2(246, 45));
 		ImGui::BeginChild("Camera TAB", ImVec2(640, 735));
 		ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-		ImGui::Checkbox("Custom FOV", &Var::BFieldOfView);
-		if (Var::BFieldOfView)
+		if (ImGui::CollapsingHeader("FieldOfView"))
 		{
-			Function::CG_FOV(Var::FFOVValue);
-			ImGui::SliderFloat("FOV Value", &Var::FFOVValue, 65, 150);
+			ImGui::Checkbox("Custom FOV", &Var::BFieldOfView);
+			if (Var::BFieldOfView)
+			{
+				Function::CG_FOV(Var::FFOVValue); //cg_fov
+				ImGui::SliderFloat("FOV Value", &Var::FFOVValue, 65, 150);
+			}
+
+			ImGui::Checkbox("Custom FOV_MIN", &Var::BCustom_FOVMIN);
+			if (Var::BCustom_FOVMIN)
+			{
+				Function::CG_FOVMIN(Var::FFOVMINValue); //cg_fovmin
+				ImGui::SliderFloat("FOV_MIN Value", &Var::FFOVMINValue, 10, 150);
+			}
 		}
+
+
+
 
 		ImGui::Checkbox("Third Person", &Var::BThirdPerson);
 
