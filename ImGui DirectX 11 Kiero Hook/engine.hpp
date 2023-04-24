4e1983e19a3f2345ab13a5abf26a1879aa5daf87
INFARCTED <93995570+INFARCTED@users.noreply.github.com>
2023-04-25 00:16:49 +0200

    tout en haut il y a la combo_box 'save/load'

--- ImGui DirectX 11 Kiero Hook/engine.hpp
+++ ImGui DirectX 11 Kiero Hook/engine.hpp
@@ -5,7 +5,124 @@ void Engine_Tab()
 		ImGui::SetCursorPos(ImVec2(246, 45));
 		ImGui::BeginChild("Editor TAB", ImVec2(640, 735));
 		ImGui::Dummy(ImVec2(0.0f, 5.0f));
+		ImGui::Text("Configuration (Save/load)");
 
+		ImGui::Combo("##settsas", &settings::save_load_num, settsName, IM_ARRAYSIZE(settsName));
+		if (ImGui::Button("Save", ImVec2(100, 20)))
+		{
+			if (settings::save_load_num == 0)
+				SaveConfiguration("C:\\Config.ini");
+			if (settings::save_load_num == 1)
+				SaveConfiguration("C:\\Config1.ini");
+			if (settings::save_load_num == 2)
+				SaveConfiguration("C:\\Config2.ini");
+			if (settings::save_load_num == 3)
+				SaveConfiguration("C:\\Config3.ini");
+			if (settings::save_load_num == 4)
+				SaveConfiguration("C:\\Config4.ini");
+			if (settings::save_load_num == 5)
+				SaveConfiguration("C:\\Config5.ini");
+			if (settings::save_load_num == 6)
+				SaveConfiguration("C:\\Config6.ini");
+			if (settings::save_load_num == 7)
+				SaveConfiguration("C:\\Config7.ini");
+			if (settings::save_load_num == 8)
+				SaveConfiguration("C:\\Config8.ini");
+			if (settings::save_load_num == 9)
+				SaveConfiguration("C:\\Config9.ini");
+			if (settings::save_load_num == 10)
+				SaveConfiguration("C:\\Config10.ini");
+			if (settings::save_load_num == 11)
+				SaveConfiguration("C:\\Config11.ini");
+			if (settings::save_load_num == 12)
+				SaveConfiguration("C:\\Config12.ini");
+			if (settings::save_load_num == 13)
+				SaveConfiguration("C:\\Config13.ini");
+			if (settings::save_load_num == 14)
+				SaveConfiguration("C:\\Config14.ini");
+			if (settings::save_load_num == 15)
+				SaveConfiguration("C:\\Config15.ini");
+			if (settings::save_load_num == 16)
+				SaveConfiguration("C:\\Config16.ini");
+			if (settings::save_load_num == 17)
+				SaveConfiguration("C:\\Config17.ini");
+			if (settings::save_load_num == 18)
+				SaveConfiguration("C:\\Config18.ini");
+			if (settings::save_load_num == 19)
+				SaveConfiguration("C:\\Config19.ini");
+			if (settings::save_load_num == 20)
+				SaveConfiguration("C:\\Config20.ini");
+			if (settings::save_load_num == 21)
+				SaveConfiguration("C:\\Config21.ini");
+			if (settings::save_load_num == 22)
+				SaveConfiguration("C:\\Config22.ini");
+			if (settings::save_load_num == 23)
+				SaveConfiguration("C:\\Config23.ini");
+			if (settings::save_load_num == 24)
+				SaveConfiguration("C:\\Config24.ini");
+			if (settings::save_load_num == 25)
+				SaveConfiguration("C:\\Config25.ini");
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Load", ImVec2(100, 20)))
+		{
+			if (settings::save_load_num == 0)
+				LoadConfiguration("C:\\Config.ini");
+			if (settings::save_load_num == 1)
+				LoadConfiguration("C:\\Config1.ini");
+			if (settings::save_load_num == 2)
+				LoadConfiguration("C:\\Config2.ini");
+			if (settings::save_load_num == 3)
+				LoadConfiguration("C:\\Config3.ini");
+			if (settings::save_load_num == 4)
+				LoadConfiguration("C:\\Config4.ini");
+			if (settings::save_load_num == 5)
+				LoadConfiguration("C:\\Config5.ini");
+			if (settings::save_load_num == 6)
+				LoadConfiguration("C:\\Config6.ini");
+			if (settings::save_load_num == 7)
+				LoadConfiguration("C:\\Config7.ini");
+			if (settings::save_load_num == 8)
+				LoadConfiguration("C:\\Config8.ini");
+			if (settings::save_load_num == 9)
+				LoadConfiguration("C:\\Config9.ini");
+			if (settings::save_load_num == 10)
+				LoadConfiguration("C:\\Config10.ini");
+			if (settings::save_load_num == 11)
+				LoadConfiguration("C:\\Config11.ini");
+			if (settings::save_load_num == 12)
+				LoadConfiguration("C:\\Config12.ini");
+			if (settings::save_load_num == 13)
+				LoadConfiguration("C:\\Config13.ini");
+			if (settings::save_load_num == 14)
+				LoadConfiguration("C:\\Config14.ini");
+			if (settings::save_load_num == 15)
+				LoadConfiguration("C:\\Config15.ini");
+			if (settings::save_load_num == 16)
+				LoadConfiguration("C:\\Config16.ini");
+			if (settings::save_load_num == 17)
+				LoadConfiguration("C:\\Config17.ini");
+			if (settings::save_load_num == 18)
+				LoadConfiguration("C:\\Config18.ini");
+			if (settings::save_load_num == 19)
+				LoadConfiguration("C:\\Config19.ini");
+			if (settings::save_load_num == 20)
+				LoadConfiguration("C:\\Config20.ini");
+			if (settings::save_load_num == 21)
+				LoadConfiguration("C:\\Config21.ini");
+			if (settings::save_load_num == 22)
+				LoadConfiguration("C:\\Config22.ini");
+			if (settings::save_load_num == 23)
+				LoadConfiguration("C:\\Config23.ini");
+			if (settings::save_load_num == 24)
+				LoadConfiguration("C:\\Config24.ini");
+			if (settings::save_load_num == 25)
+				LoadConfiguration("C:\\Config25.ini");
+
+
+		}
+
+		ImGui::Separator(); ImGui::Text("Dark Edit");
 		if (ImGui::Button("Dark Map (Simple)"))
 		{
 			Function::DarkMap(1);
@@ -206,6 +323,16 @@ void Engine_Tab()
 			if (ImGui::Button("Apply Rotation"))
 			{
 				Function::SkyRotation(Var::SkyRotation_a1);
+			}
+		}
+
+		ImGui::Checkbox("Sun Light", &Var::BCustom_Lightsun);
+		if (Var::BCustom_Lightsun)
+		{
+			ImGui::SliderFloat("SunLight a1 (float)", &Var::SkyLight_a1, 0.1, 20);
+			if (ImGui::Button("Apply SunLight"))
+			{
+				Function::SunLight(Var::SkyLight_a1);
 			}
 		}
 
