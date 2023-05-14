442c7e831d4df35e65aafc7142a21d26c7e6e407
INFARCTED <93995570+INFARCTED@users.noreply.github.com>
2023-05-14 19:30:01 +0200

    Update Offset.hpp (plutonium update (14.05.23))

--- ImGui DirectX 11 Kiero Hook/Offset.hpp
+++ ImGui DirectX 11 Kiero Hook/Offset.hpp
@@ -1,18 +1,18 @@
 
 namespace Offset {
 	//Server
-	uintptr_t sv_cheats = 0x2A20AA0;
+	uintptr_t sv_cheats = 0x2A20B00;
 	//Camera
-	uintptr_t cg_fov = 0x2A413E0;
-	uintptr_t cg_fovmin = 0x2A41440;
-	uintptr_t cg_thirdperson = 0x2A45BE0;
+	uintptr_t cg_fov = 0x2A41440;
+	uintptr_t cg_fovmin = 0x2A414A0;
+	uintptr_t cg_thirdperson = 0x2A45C40;
 	//vue
-	uintptr_t pitch_down = 0x2A28720;
-	uintptr_t pitch_up = 0x2A286C0;
+	uintptr_t pitch_down = 0x2A28780;
+	uintptr_t pitch_up = 0x2A28720;
 	//Visual
-	uintptr_t cg_usecolorcontrol = 0xD76DAC; //00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 91 39 08 00 A0 1F 05 00 00 00 00 00 A8 05 2F 1A
+	uintptr_t cg_usecolorcontrol = 0x2A44860;
 	//Overlay
-	uintptr_t Is_InGame = 0xD76DB0;
+	uintptr_t Is_InGame = 0xD76DB0; //00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 91 39 08 00 A0 1F 05 00 00 00 00 00 A8 05 2F 1A
 	//Pre-Game
 	uintptr_t Prestige = 0x02F90754;
 	
@@ -69,50 +69,50 @@ namespace Function {
 
 	void vc_fbm(float a1, float a2, float a3, float a4)
 	{
+		*(float*)0x2A3CD00 = a1;
+		*(float*)(0x2A3CD00 + 0x4) = a2;
+		*(float*)(0x2A3CD00 + 0x8) = a3;
+		*(float*)(0x2A3CD00 + 0xC) = a4;
+	}
+	void vc_fsm(float a1, float a2, float a3, float a4)
+	{
 		*(float*)0x2A3CCA0 = a1;
 		*(float*)(0x2A3CCA0 + 0x4) = a2;
 		*(float*)(0x2A3CCA0 + 0x8) = a3;
 		*(float*)(0x2A3CCA0 + 0xC) = a4;
 	}
-	void vc_fsm(float a1, float a2, float a3, float a4)
-	{
-		*(float*)0x2A3CC40 = a1;
-		*(float*)(0x2A3CC40 + 0x4) = a2;
-		*(float*)(0x2A3CC40 + 0x8) = a3;
-		*(float*)(0x2A3CC40 + 0xC) = a4;
-	}
 	void vc_rgbh(float a1, float a2, float a3, float a4)
-	{
-		*(float*)0x2A3C4C0 = a1;
-		*(float*)(0x2A3C4C0 + 0x4) = a2;
-		*(float*)(0x2A3C4C0 + 0x8) = a3;
-		*(float*)(0x2A3C4C0 + 0xC) = a4;
-	}
-	void vc_yl(float a1, float a2, float a3, float a4)
-	{
-		*(float*)0x2A3C5E0 = a1;
-		*(float*)(0x2A3C5E0 + 0x4) = a2;
-		*(float*)(0x2A3C5E0 + 0x8) = a3;
-		*(float*)(0x2A3C5E0 + 0xC) = a4;
-	}
-	void vc_yh(float a1, float a2, float a3, float a4)
-	{
-		*(float*)0x2A3C580 = a1;
-		*(float*)(0x2A3C580 + 0x4) = a2;
-		*(float*)(0x2A3C580 + 0x8) = a3;
-		*(float*)(0x2A3C580 + 0xC) = a4;
-	}
-	void vc_rgbl(float a1, float a2, float a3, float a4)
 	{
 		*(float*)0x2A3C520 = a1;
 		*(float*)(0x2A3C520 + 0x4) = a2;
 		*(float*)(0x2A3C520 + 0x8) = a3;
 		*(float*)(0x2A3C520 + 0xC) = a4;
 	}
+	void vc_yl(float a1, float a2, float a3, float a4)
+	{
+		*(float*)0x2A3C640 = a1;
+		*(float*)(0x2A3C640 + 0x4) = a2;
+		*(float*)(0x2A3C640 + 0x8) = a3;
+		*(float*)(0x2A3C640 + 0xC) = a4;
+	}
+	void vc_yh(float a1, float a2, float a3, float a4)
+	{
+		*(float*)0x2A3C5E0 = a1;
+		*(float*)(0x2A3C5E0 + 0x4) = a2;
+		*(float*)(0x2A3C5E0 + 0x8) = a3;
+		*(float*)(0x2A3C5E0 + 0xC) = a4;
+	}
+	void vc_rgbl(float a1, float a2, float a3, float a4)
+	{
+		*(float*)0x2A3C580 = a1;
+		*(float*)(0x2A3C580 + 0x4) = a2;
+		*(float*)(0x2A3C580 + 0x8) = a3;
+		*(float*)(0x2A3C580 + 0xC) = a4;
+	}
 
 	void SunLight(float light)
 	{
-		*(float*)0x2A39880 = light;
+		*(float*)0x2A398E0 = light;
 	}
 	void DarkMap(int mode)
 	{
@@ -120,18 +120,18 @@ namespace Function {
 		{
 		
 	
-			*(float*)0x2A38C20 = -1000; //r_lodBiasRigid
-			*(float*)0x2A38CE0 = -1000; //r_lodBiasSkinned
-			*(float*)0x2A3F1C0 = 1; //r_skyTransition
-			*(float*)0x2A3E800 = 3.9; //r_exposureValue
-			*(float*)0x2A39880 = 1; //r_lightTweakSunLight
-			*(float*)0x2A40360 = 0; //r_sky_intensity_factor0
+			*(float*)0x2A38C80 = -1000; //r_lodBiasRigid
+			*(float*)0x2A38D40 = -1000; //r_lodBiasSkinned
+			*(float*)0x2A3F220 = 1; //r_skyTransition
+			*(float*)0x2A3E860 = 3.9; //r_exposureValue
+			*(float*)0x2A398E0 = 1; //r_lightTweakSunLight
+			*(float*)0x2A403C0 = 0; //r_sky_intensity_factor0
 
-			*(int*)0x2A3D240 = 0; //r_dof_enable
-			*(int*)0x2A3AF60 = 2; //sm_sunquality
-			*(int*)0x2A3C1C0 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
-			*(int*)0x2A3C640 = 1; //r_bloomTweaks
-			*(int*)0x2A3E7A0 = 1; //r_exposureTweak
+			*(int*)0x2A3D2A0 = 0; //r_dof_enable
+			*(int*)0x2A3AFC0 = 2; //sm_sunquality
+			*(int*)0x2A3C220 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
+			*(int*)0x2A3C6A0 = 1; //r_bloomTweaks
+			*(int*)0x2A3E800 = 1; //r_exposureTweak
 			
 
 			vc_fbm(0, 0, 0, 0);
@@ -146,18 +146,18 @@ namespace Function {
 		if (mode == 2) //Yellow
 		{
 
-			*(float*)0x2A38C20 = -1000; //r_lodBiasRigid
-			*(float*)0x2A38CE0 = -1000; //r_lodBiasSkinned
-			*(float*)0x2A3F1C0 = 1; //r_skyTransition
-			*(float*)0x2A3E800 = 3.9; //r_exposureValue
-			*(float*)0x2A39880 = 1; //r_lightTweakSunLight
-			*(float*)0x2A40360 = 0; //r_sky_intensity_factor0
+			*(float*)0x2A38C80 = -1000; //r_lodBiasRigid
+			*(float*)0x2A38D40 = -1000; //r_lodBiasSkinned
+			*(float*)0x2A3F220 = 1; //r_skyTransition
+			*(float*)0x2A3E860 = 3.9; //r_exposureValue
+			*(float*)0x2A398E0 = 1; //r_lightTweakSunLight
+			*(float*)0x2A403C0 = 0; //r_sky_intensity_factor0
 
-			*(int*)0x2A3D240 = 0; //r_dof_enable
-			*(int*)0x2A3AF60 = 2; //sm_sunquality
-			*(int*)0x2A3C1C0 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
-			*(int*)0x2A3C640 = 1; //r_bloomTweaks
-			*(int*)0x2A3E7A0 = 1; //r_exposureTweak
+			*(int*)0x2A3D2A0 = 0; //r_dof_enable
+			*(int*)0x2A3AFC0 = 2; //sm_sunquality
+			*(int*)0x2A3C220 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
+			*(int*)0x2A3C6A0 = 1; //r_bloomTweaks
+			*(int*)0x2A3E800 = 1; //r_exposureTweak
 
 
 			vc_fbm(0, 0, 0, 0);
@@ -172,18 +172,18 @@ namespace Function {
 		if (mode == 3) //Red
 		{
 
-			*(float*)0x2A38C20 = -1000; //r_lodBiasRigid
-			*(float*)0x2A38CE0 = -1000; //r_lodBiasSkinned
-			*(float*)0x2A3F1C0 = 1; //r_skyTransition
-			*(float*)0x2A3E800 = 3.9; //r_exposureValue
-			*(float*)0x2A39880 = 1; //r_lightTweakSunLight
-			*(float*)0x2A40360 = 0; //r_sky_intensity_factor0
+			*(float*)0x2A38C80 = -1000; //r_lodBiasRigid
+			*(float*)0x2A38D40 = -1000; //r_lodBiasSkinned
+			*(float*)0x2A3F220 = 1; //r_skyTransition
+			*(float*)0x2A3E860 = 3.9; //r_exposureValue
+			*(float*)0x2A398E0 = 1; //r_lightTweakSunLight
+			*(float*)0x2A403C0 = 0; //r_sky_intensity_factor0
 
-			*(int*)0x2A3D240 = 0; //r_dof_enable
-			*(int*)0x2A3AF60 = 2; //sm_sunquality
-			*(int*)0x2A3C1C0 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
-			*(int*)0x2A3C640 = 1; //r_bloomTweaks
-			*(int*)0x2A3E7A0 = 1; //r_exposureTweak
+			*(int*)0x2A3D2A0 = 0; //r_dof_enable
+			*(int*)0x2A3AFC0 = 2; //sm_sunquality
+			*(int*)0x2A3C220 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
+			*(int*)0x2A3C6A0 = 1; //r_bloomTweaks
+			*(int*)0x2A3E800 = 1; //r_exposureTweak
 
 
 			vc_fbm(0, 0, 0, 0);
@@ -198,18 +198,18 @@ namespace Function {
 		if (mode == 4) //Green
 		{
 
-			*(float*)0x2A38C20 = -1000; //r_lodBiasRigid
-			*(float*)0x2A38CE0 = -1000; //r_lodBiasSkinned
-			*(float*)0x2A3F1C0 = 1; //r_skyTransition
-			*(float*)0x2A3E800 = 3.9; //r_exposureValue
-			*(float*)0x2A39880 = 1; //r_lightTweakSunLight
-			*(float*)0x2A40360 = 0; //r_sky_intensity_factor0
+			*(float*)0x2A38C80 = -1000; //r_lodBiasRigid
+			*(float*)0x2A38D40 = -1000; //r_lodBiasSkinned
+			*(float*)0x2A3F220 = 1; //r_skyTransition
+			*(float*)0x2A3E860 = 3.9; //r_exposureValue
+			*(float*)0x2A398E0 = 1; //r_lightTweakSunLight
+			*(float*)0x2A403C0 = 0; //r_sky_intensity_factor0
 
-			*(int*)0x2A3D240 = 0; //r_dof_enable
-			*(int*)0x2A3AF60 = 2; //sm_sunquality
-			*(int*)0x2A3C1C0 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
-			*(int*)0x2A3C640 = 1; //r_bloomTweaks
-			*(int*)0x2A3E7A0 = 1; //r_exposureTweak
+			*(int*)0x2A3D2A0 = 0; //r_dof_enable
+			*(int*)0x2A3AFC0 = 2; //sm_sunquality
+			*(int*)0x2A3C220 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
+			*(int*)0x2A3C6A0 = 1; //r_bloomTweaks
+			*(int*)0x2A3E800 = 1; //r_exposureTweak
 
 
 			vc_fbm(0, 0, 0, 0);
@@ -224,18 +224,18 @@ namespace Function {
 		if (mode == 5) //Cyan
 		{
 
-			*(float*)0x2A38C20 = -1000; //r_lodBiasRigid
-			*(float*)0x2A38CE0 = -1000; //r_lodBiasSkinned
-			*(float*)0x2A3F1C0 = 1; //r_skyTransition
-			*(float*)0x2A3E800 = 3.9; //r_exposureValue
-			*(float*)0x2A39880 = 1; //r_lightTweakSunLight
-			*(float*)0x2A40360 = 0; //r_sky_intensity_factor0
+			*(float*)0x2A38C80 = -1000; //r_lodBiasRigid
+			*(float*)0x2A38D40 = -1000; //r_lodBiasSkinned
+			*(float*)0x2A3F220 = 1; //r_skyTransition
+			*(float*)0x2A3E860 = 3.9; //r_exposureValue
+			*(float*)0x2A398E0 = 1; //r_lightTweakSunLight
+			*(float*)0x2A403C0 = 0; //r_sky_intensity_factor0
 
-			*(int*)0x2A3D240 = 0; //r_dof_enable
-			*(int*)0x2A3AF60 = 2; //sm_sunquality
-			*(int*)0x2A3C1C0 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
-			*(int*)0x2A3C640 = 1; //r_bloomTweaks
-			*(int*)0x2A3E7A0 = 1; //r_exposureTweak
+			*(int*)0x2A3D2A0 = 0; //r_dof_enable
+			*(int*)0x2A3AFC0 = 2; //sm_sunquality
+			*(int*)0x2A3C220 = 0; //r_filmUseTweaks (je sais pas pourquoi, quand je met 1 il fait un black screen)
+			*(int*)0x2A3C6A0 = 1; //r_bloomTweaks
+			*(int*)0x2A3E800 = 1; //r_exposureTweak
 
 
 			vc_fbm(0, 0, 0, 0);
@@ -251,24 +251,24 @@ namespace Function {
 
 	void chams(float a1, float a2, float a3)
 	{
-		*(float*)0x2A3EBC0 = a1;
-		*(float*)(0x2A3EBC0 + 0x4) = a2;
-		*(float*)(0x2A3EBC0 + 0x8) = a3;
+		*(float*)0x2A3EC20 = a1;
+		*(float*)(0x2A3EC20 + 0x4) = a2;
+		*(float*)(0x2A3EC20 + 0x8) = a3;
 	}
 
 	void WaterSheetingScreenFX(bool value)
 	{
-		*(int*)0x2A3D8A0 = value;
+		*(int*)0x2A3D900 = value;
 	}
 
 	void PoisonScreenFX(bool value)
 	{
-		*(int*)0x2A3E020 = value;
+		*(int*)0x2A3E080 = value;
 	}
 
 	void SkyRotation(float rotation)
 	{
-		*(float*)0x2A3F220 = rotation;
+		*(float*)0x2A3F280 = rotation;
 	}
 
 
@@ -277,12 +277,6 @@ namespace Function {
 		*(float*)Offset::pitch_down = down;
 		*(float*)Offset::pitch_up = up;
 	}
-
-
-//	const static auto ExecuteServerCMD = reinterpret_cast<std::uintptr_t(__fastcall*)(const char*)>(0x630650); ///5B3070
-	
-	//const static auto Cbuf_AddText = reinterpret_cast<std::uintptr_t(__fastcall*)(int, const char*)>(0xD6FE00); //C25A04 //(je l'enlève de la source !)
-
 
 }
 
